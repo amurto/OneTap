41c11144deed923ceb3ce1b4e63ef0339e36e4e4
amurto <amurtobasu@gmail.com>
2020-02-05 22:55:45 +0530

    Checkpoint3

---
+++ admin/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PrimaryAppBar from './components/PrimaryAppBar';
+
+const App = () => {
+  return (
+    <div>
+      <PrimaryAppBar />
+    </div>
+  );
+}
+
+export default App;
